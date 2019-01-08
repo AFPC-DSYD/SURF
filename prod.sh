f86d86796590dd2d245437dc80979a03fcc0fef5
mofongo74 <mad.gad.gomez@gmail.com>
2019-01-08 13:44:03 -0600

    Further modified prod.sh.

--- prod.sh
+++ prod.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-#begin by resetting ps_prod working directory to last commit state
+#begin by resetting SURF working directory to last commit state
 git -C ../SURF_PROD reset --hard;
-#git pull to sync with remote
+#git pull to sync with remote to avoid collisions
 git -C ../SURF_PROD pull origin master;
-#git pull origin master
+
 #build for prod (need prod endpoints)
 yarn run build;
-#mv ./fonts ./dist/static/.;
-#rm ./dist/static/js/*.map;
-#rm ./dist/static/*.png;
-#rm ./dist/static/*.jpg;
-#rm ./dist/static/*.JPG;
 
-#get most recent commit and current branch in perstat
+#get most recent commit and current branch in SURF
 lastCommitHash=$(git rev-parse --short HEAD)
 lastCommit=$(git log -n 1 --oneline)
 branchName=$(git rev-parse --abbrev-ref HEAD)
